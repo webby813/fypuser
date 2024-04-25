85b2cf099916b72beabf4bc39db323669c056c18
webby813 <webbyteo02@gmail.com>
2024-04-25 14:44:07 +0800

    - Migrate [Login] from realtime-db to firestore

--- lib/Firebase/retrieve_data.dart
+++ lib/Firebase/retrieve_data.dart
@@ -1,54 +1,41 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fypuser/home.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/material.dart';
+import '../Components/alertDialog_widget.dart';
 import '../SharedPref/user_pref.dart';
 
-class Identify{
-  final dbRef = FirebaseDatabase.instance.ref().child('User');
+class Checking{
+  void checkCredential(context, String email, String password)async{
+    final db = FirebaseFirestore.instance;
 
-  List dataList = [];
-  List dataKeyList = [];
-
-  Future<void> loginCheck(context, String username, String password) async{
-    dbRef.onValue.listen((event) {
-      for(final child in event.snapshot.children){
-        Map<dynamic,dynamic> obj = child.value as Map<dynamic, dynamic>;
-        dataList.add(obj);
-        dataKeyList.add(obj);
-        for(var i=0; i!= dataList.length;i++){
-        }
-
-        try{
-          for(var i=0; i!= dataList.length;i++){
-            if(username != dataList[i]["Username"] || password != dataList[i]['Password']){
-              // print('Unsuccessful');
-            }
-            else if(username == dataList[i]['Username'] && password == dataList[i]['Password']){
-              Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => const Home()));
-              setSharedPref(username, password);
-              saveLoggedInFlag();
-              ///implement shared preferences here
-              dataKeyList.clear();
-              dataList.clear();
-              // print("Successful");
-              break;
-            }
-            else{
-              break;
-            }
-          }
-        }catch(error){
-          // print(error);
-        }
-      }
+    Future<void> saveLoggedInFlag() async {
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      await prefs.setBool('isLoggedIn', true);
     }
-    );
-  }
 
-  Future<void> saveLoggedInFlag() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    await prefs.setBool('isLoggedIn', true);
+    try{
+      QuerySnapshot snapshot = await db.collection('users').where('email', isEqualTo: email).where('password', isEqualTo: password).get();
+      if(snapshot.docs.isNotEmpty){
+        Navigator.pushReplacement(context, MaterialPageRoute(builder: (_) => const Home()));
+        setSharedPref(email, password);
+        saveLoggedInFlag();
+      }
+      else{
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return const AlertDialogWidget(
+              title: 'Error',
+              content: 'Please ensure correct login credential',
+            );
+          },
+        );
+      }
+    }catch(e){
+      // print(e);
+    }
   }
 }
 
